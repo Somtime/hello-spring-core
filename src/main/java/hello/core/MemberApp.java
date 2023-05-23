9431fe5b8cc55103a3056f6e27571eb489ff3698
Somtime <rghee0821@gmail.com>
2023-05-23 18:05:10 +0900

    섹션 3. 스프링 핵심 원리 이해2 - 객체 지향 원리 적용
    3-1) 새로운 할인 정책 개발
    3-2) 새로운 할인 정책 적용과 문제점
    3-3) 관심사의 분리
    3-4) AppConfig 리팩터링
    
    이전까지 ServiceImpl 이 다른 Service 나 Repository 의 구현체들에 의존하던 것을
    AppConfig 에서 구현체들을 주입하게 수정함으로써 ServiceImpl 이 추상화에만 의존하고
    구현체에는 의존하지 않게 함

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -7,11 +7,13 @@ import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
         Member member = new Member(1L, "member1", Grade.VIP);
         memberService.join(member);
 
         Member findMember = memberService.findMember(1L);
-        System.out.println("find member test: " + member.getName() == findMember.getName());
+        System.out.println("new member: " + member.getName());
+        System.out.println("findMember: " + findMember.getName());
     }
 }
