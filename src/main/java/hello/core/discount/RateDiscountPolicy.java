d8ffbc6f4093ce34b0da970299a3888191fb7764
Somtime <rghee0821@gmail.com>
2023-06-08 17:47:37 +0900

    섹션 7. 의존관계 자동 주입
    7-1) 다양한 의존관계 주입방법
    7-2) 옵션 처리
    7-3) 생성자 주입을 선택해라!
    7-4) 롬복과 최신 트랜드
    7-5) 조회 빈이 2개 이상 - 문제
    7-6) @Autowired 필드 명, @Qualifier, @Primary
    7-7) 애노테이션 직접 만들기
    7-8) 조회한 빈이 모두 필요할 때, List, Map
    7-9) 자동, 수동의 올바른 실무 운영 기준
    
    @Qualifier, @Primary Annotation 사용법
    Lombok 사용법 (Get/Setter, Constructor)
    List, Map 으로 생성자 주입으로 여러 구현체들을 사용하는 방법 -> 실무에서 결제 구현에서 사용할만한 힌트를 얻었음

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,16 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("rateDiscountPolicy") // 생성자나 수정자 등록 시 @Qualifier 에 등록된 이름의 빈을 찾음
+@MainDiscountPolicy
+//@Primary
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10;
     @Override
